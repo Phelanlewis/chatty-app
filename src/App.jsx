8569c6ee2716dbb1892818108e35277cb3bb5f12
Phelan Lewis <phelanlewis@gmail.com>
2017-02-07 18:28:39 +0000

    Got components to render

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
+import Navbar from './navbar.jsx'
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <Navbar />
+        <MessageList />
+        <ChatBar />
+      </div>
     );
   }
 }
